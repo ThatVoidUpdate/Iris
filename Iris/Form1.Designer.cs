0c170d76c11c3db43c20d3243b2a209a904f26c9
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-08-28 23:42:21 +0100

    Created basic functionality to decode an image in the bin folder

--- Iris/Form1.Designer.cs
+++ Iris/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnl_Controls = new System.Windows.Forms.Panel();
+            this.pbx_Image = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_Controls
+            // 
+            this.pnl_Controls.Location = new System.Drawing.Point(519, 13);
+            this.pnl_Controls.Name = "pnl_Controls";
+            this.pnl_Controls.Size = new System.Drawing.Size(270, 500);
+            this.pnl_Controls.TabIndex = 0;
+            // 
+            // pbx_Image
+            // 
+            this.pbx_Image.Location = new System.Drawing.Point(13, 13);
+            this.pbx_Image.Name = "pbx_Image";
+            this.pbx_Image.Size = new System.Drawing.Size(500, 500);
+            this.pbx_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx_Image.TabIndex = 1;
+            this.pbx_Image.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(802, 525);
+            this.Controls.Add(this.pbx_Image);
+            this.Controls.Add(this.pnl_Controls);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Image)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnl_Controls;
+        private System.Windows.Forms.PictureBox pbx_Image;
     }
 }
 
--- Iris/Form1.cs
+++ Iris/Form1.cs
@@ -7,14 +7,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Iris
 {
     public partial class Form1 : Form
     {
+        public byte[] FileBytes;
+        public int LabelSize;
+
+        Image convertedImage;
+        Bitmap ConvertedBitmap;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pnl_Image_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //load image
+            FileBytes = File.ReadAllBytes("TestImg.IMG");
+
+            //decode image
+            string fileString = Encoding.Default.GetString(FileBytes);
+
+            Regex NumberRegex = new Regex(@"\d+", RegexOptions.Compiled);
+
+            //Find the metadata length
+            Regex LabelSizeRegex = new Regex(@"LBLSIZE=\d*\s", RegexOptions.Compiled);
+            MatchCollection matches = LabelSizeRegex.Matches(fileString);
+
+            int MetadataLength = Convert.ToInt32(NumberRegex.Matches(matches[0].Value)[0].Value);
+            Console.WriteLine($"Metadata length: {MetadataLength}");
+            string Metadata = fileString.Substring(0, MetadataLength);
+
+            /*
+             * Get width and height:
+            width = re.findall(r"RECSIZE=\d*\s", Metadata)[0]
+            width = int(re.findall(r"\d+", width)[0])
+
+            height = re.findall(r"NL=\d*\s", Metadata)[0]
+            height = int(re.findall(r"\d+", height)[0])
+            */
+
+            Regex WidthRegex = new Regex(@"RECSIZE=\d*\s", RegexOptions.Compiled);
+            matches = WidthRegex.Matches(Metadata);
+            int Width = Convert.ToInt32(NumberRegex.Matches(matches[0].Value)[0].Value);
+
+            Regex HeightRegex = new Regex(@"NL=\d*\s", RegexOptions.Compiled);
+            matches = HeightRegex.Matches(Metadata);
+            int Height = Convert.ToInt32(NumberRegex.Matches(matches[0].Value)[0].Value);
+
+            Console.WriteLine($"Dimensions: {Width}x{Height}");
+
+            /*
+             * Get Target
+            Target = re.findall(r"TARGET_NAME='[\s\w]+'", Metadata)[0]
+            Target= Target.split('=')[1][1:-1]
+            */
+
+            Regex TargetRegex = new Regex(@"TARGET_NAME='[\s\w]+'", RegexOptions.Compiled);
+            matches = TargetRegex.Matches(Metadata);
+            String Target = matches[0].Value;
+
+            Console.WriteLine($"Target: {Target.Split('=')[1]}");
+
+
+            //write image to pbx_Image
+            ConvertedBitmap = new Bitmap(Width, Height);
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    Color color = Color.FromArgb(FileBytes[MetadataLength + (y * Width) + x], FileBytes[MetadataLength + (y * Width) + x], FileBytes[MetadataLength + (y * Width) + x]);
+                    ConvertedBitmap.SetPixel(x, y, color);
+                }
+            }
+
+            pbx_Image.Image = ConvertedBitmap;
+
         }
     }
 }
