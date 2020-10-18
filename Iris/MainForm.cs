38c378529680e190f6cd7e871b0541e214b9ec41
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-10-18 22:55:42 +0100

    Added Voyager Decoder

--- Iris/MainForm.cs
+++ Iris/MainForm.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
-using System.Text.RegularExpressions;
 using Iris.Decoders;
 
 namespace Iris
@@ -19,7 +10,7 @@ namespace Iris
         public MainForm()
         {
             InitializeComponent();
-            SelectedDecoder = new CassiniDecoder();
+            SelectedDecoder = new VoyagerDecoder();
         }
         
 
